1b30358946fd2f655227bbe8ac78db6fe855ccec
Modini <modini@gmail.com>
2026-02-23 11:10:26 +0530

    Completed ROS2 launch files assignment with publisher, subscriber and launch file

---
+++ launch/pubsub.launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    talker_node = Node(
+        package='ros2_launch_demo',
+        executable='talker',
+        name='talker',
+        parameters=[
+            {'message_prefix': 'ROS2'}
+        ]
+    )
+
+    listener_node = Node(
+        package='ros2_launch_demo',
+        executable='listener',
+        name='listener'
+    )
+
+    return LaunchDescription([
+        talker_node,
+        listener_node
+    ])
